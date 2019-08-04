786c365184faa5727159ccf804538f560e73a407
Futa Hirakoba <korosuke613613@gmail.com>
2019-08-05 01:14:13 +0900

    ちょっとテストを追加

--- src/test/test_game.py
+++ src/test/test_game.py
@@ -39,3 +39,37 @@ def test_listen_usi_failed(capfd, monkeypatch, game):
     game.listen_usi(is_test=True)
     out, err = capfd.readouterr()
     assert out == "Don't correct usi!\n"
+
+
+def test_start(game, capfd):
+    def listen_usi_generator():
+        for l in ["7g7f", "3c3d"]:
+            yield l
+
+    def listen_usi_mock():
+        return listen_usi.__next__()
+
+    listen_usi = listen_usi_generator()
+    game.listen_usi = listen_usi_mock
+
+    try:
+        game.start()
+    except StopIteration:
+        out, err = capfd.readouterr()
+        game.board.print()
+        out, err = capfd.readouterr()
+        assert out == """後手の持駒：
+  ９ ８ ７ ６ ５ ４ ３ ２ １
++---------------------------+
+|v香v桂v銀v金v玉v金v銀v桂v香|一
+| ・v飛 ・ ・ ・ ・ ・v角 ・|二
+|v歩v歩v歩v歩v歩v歩 ・v歩v歩|三
+| ・ ・ ・ ・ ・ ・v歩 ・ ・|四
+| ・ ・ ・ ・ ・ ・ ・ ・ ・|五
+| ・ ・ 歩 ・ ・ ・ ・ ・ ・|六
+| 歩 歩 ・ 歩 歩 歩 歩 歩 歩|七
+| ・ 角 ・ ・ ・ ・ ・ 飛 ・|八
+| 香 桂 銀 金 玉 金 銀 桂 香|九
++---------------------------+
+先手の持駒：
+"""
